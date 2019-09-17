5506d3e077dcbfd54ef38ce0d1bbbaae5e266dd9
Renu <preetha_fam@hotmail.com>
2019-09-16 19:08:37 -0500

    completed mvc persistence

---
+++ src/main/java/org/launchcode/models/forms/AddMenuItemForm.java
@@ -0,0 +1,55 @@
+package org.launchcode.models.forms;
+
+import org.launchcode.models.Cheese;
+import org.launchcode.models.Menu;
+
+import javax.validation.constraints.NotNull;
+
+public class AddMenuItemForm {
+    private Menu menu;
+    private Iterable<Cheese>cheeses;
+
+    @NotNull
+    private int menuId;
+    @NotNull
+    private int cheeseId;
+
+    public AddMenuItemForm(Iterable<Cheese> all, Menu menu) {
+    }
+
+    public AddMenuItemForm(Menu menu, Iterable<Cheese> cheeses) {
+        this.menu = menu;
+        this.cheeses=cheeses;
+    }
+
+    public AddMenuItemForm() {
+    }
+
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public Iterable<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public void setMenuId(int menuId) {
+        this.menuId = menuId;
+    }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
+    }
+
+    public int getMenuId() {
+        return menuId;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
+    }
+}
+
+
+
